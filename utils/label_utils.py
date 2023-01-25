9fd8c776676adf60255d783ba7a0c09d0205c1f9
jonathan-kelvin <jonathankelvinsantoso@gmail.com>
2023-01-26 02:10:21 +0800

    fixed commits

--- utils/label_utils.py
+++ utils/label_utils.py
@@ -9,6 +9,7 @@ import argparse
 import zipfile
 import shutil
 import os.path
+import pandas as pd
 
 def rename(input_dir, prefix=None, output_dir=None):
     """
@@ -31,3 +32,30 @@ def rename(input_dir, prefix=None, output_dir=None):
         new_file_name = "{}_{}{}".format(prefix, count, file_ext)
         new_file_path = os.path.join(output_dir, new_file_name)
         shutil.copy(img, new_file_path)
+
+def generate_csv(path, output_dir=None, csv_name='label.csv'):
+    """
+    creates a csv file with 2 columns, file_name and label.
+    expects data to be in format:
+        data
+        --real
+        ----real_123.png
+        --fake
+        ----fake_123.png
+    Args:
+        path -> path to data
+    """
+    if output_dir is None:
+        output_dir = path
+    df = pd.DataFrame(columns=['file_name', 'label'])
+    real_path = os.path.join(path, 'real')
+    fake_path = os.path.join(path, 'fake')
+    for f in tqdm(os.listdir(real_path)):
+        if f.startswith('real'):
+            newRow = {'file_name': f, 'label': 'real'}
+            df = df.append(newRow, ignore_index=True)
+    for f in tqdm(os.listdir(fake_path)):
+        if f.startswith('fake'):
+            newRow = {'file_name': f, 'label': 'fake'}
+            df = df.append(newRow, ignore_index=True)
+    df.to_csv(os.path.join(output_dir, csv_name))
